82582cc676cb594a6cea6b817021ad641c5a95c3
Mark <16828892+Mark-CLi@users.noreply.github.com>
2025-12-08 15:32:38 -0800

    add Login and SignUp pages; update dependencies for authentication

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,6 +3,8 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { Box, Container, Typography } from '@mui/material';
 import Header from './components/Header';
 import Home from './pages/Home';
+import Login from './pages/Login';
+import SignUp from './pages/SignUp';
 import ROUTES from './config/routes';
 
 function App() {
