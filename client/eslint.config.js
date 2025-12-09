b88e5237721eaaf69498f4b00e63c88bc7c2f600
Mark <16828892+Mark-CLi@users.noreply.github.com>
2025-12-08 17:02:43 -0800

    prettier autofix

--- client/eslint.config.js
+++ client/eslint.config.js
@@ -1,38 +1,38 @@
-import js from "@eslint/js";
-import globals from "globals";
-import tseslint from "typescript-eslint";
-import pluginReact from "eslint-plugin-react";
-import pluginReactHooks from "eslint-plugin-react-hooks";
+import js from '@eslint/js'
+import globals from 'globals'
+import tseslint from 'typescript-eslint'
+import pluginReact from 'eslint-plugin-react'
+import pluginReactHooks from 'eslint-plugin-react-hooks'
 
 export default [
-  {
-    ignores: ["dist/**", "node_modules/**", "*.config.js", "*.config.ts"]
-  },
-  js.configs.recommended,
-  ...tseslint.configs.recommended,
-  {
-    files: ["**/*.{js,mjs,cjs,ts,mts,cts,jsx,tsx}"],
-    plugins: {
-      react: pluginReact,
-      "react-hooks": pluginReactHooks
+    {
+        ignores: ['dist/**', 'node_modules/**', '*.config.js', '*.config.ts'],
     },
-    languageOptions: {
-      globals: globals.browser,
-      parserOptions: {
-        ecmaFeatures: {
-          jsx: true
-        }
-      }
+    js.configs.recommended,
+    ...tseslint.configs.recommended,
+    {
+        files: ['**/*.{js,mjs,cjs,ts,mts,cts,jsx,tsx}'],
+        plugins: {
+            react: pluginReact,
+            'react-hooks': pluginReactHooks,
+        },
+        languageOptions: {
+            globals: globals.browser,
+            parserOptions: {
+                ecmaFeatures: {
+                    jsx: true,
+                },
+            },
+        },
+        settings: {
+            react: {
+                version: 'detect',
+            },
+        },
+        rules: {
+            ...pluginReact.configs.recommended.rules,
+            ...pluginReactHooks.configs.recommended.rules,
+            'react/react-in-jsx-scope': 'off',
+        },
     },
-    settings: {
-      react: {
-        version: "detect"
-      }
-    },
-    rules: {
-      ...pluginReact.configs.recommended.rules,
-      ...pluginReactHooks.configs.recommended.rules,
-      "react/react-in-jsx-scope": "off"
-    }
-  }
-];
+]
